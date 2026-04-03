c918fcbe0e01cb7f6a6ecac85d7158b4ca028a9f
Sai Sashreek Dittakavi <saisashreekd@gmail.com>
2026-04-03 19:06:56 +0530

    Tailwindcss Version 3 upgraded to version 4 with fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl text-teal-500">
+      <h1 className="text-5xl text-teal-500 font-semibold text-center">
         Welcome to AI Smart Job Tracker!!!
       </h1>
     </>
